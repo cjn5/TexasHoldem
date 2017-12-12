0d7c3046d942940bd9721f41208b1843e2df8539
cjn5 <32108750+cjn5@users.noreply.github.com>
2017-12-12 13:03:33 -0600

    Add files via upload

--- Game.java
+++ Game.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class Game {
 	public String[] board;
 	public ArrayList<Player> players;
+	public int highHand;
 	public Game(int otherPlayers) {
+		players = new ArrayList<Player>();
 		board = new String[5];
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your player name");
@@ -21,6 +23,14 @@ public class Game {
 	public void setBoard(int index, String card) {
 		board[index] = card;
 	}
+	public void printBoard(int howMany) {
+		int count = 0;
+		while(count < howMany) {
+			System.out.print(board[count] + " ");
+			count++;
+		}
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -28,26 +38,50 @@ public class Game {
 		int n = sc.nextInt();
 		Game texas = new Game(n);
 		ArrayList<String> list = new ArrayList<String>();
-		for (int i = 1; i <= 13; i++) {
-			list.add(Integer.toString(i) + "s");
+		for (int i = 2; i <= 14; i++) {
+			list.add("s" + Integer.toString(i));
 		}
-		for (int i = 1; i <= 13; i++) {
-			list.add(Integer.toString(i) + "h");
+		for (int i = 2; i <= 14; i++) {
+			list.add("h" + Integer.toString(i));
 		}
-		for (int i = 1; i <= 13; i++) {
-			list.add(Integer.toString(i) + "c");
+		for (int i = 2; i <= 14; i++) {
+			list.add("c" + Integer.toString(i));
 		}
-		for (int i = 1; i <= 13; i++) {
-			list.add(Integer.toString(i) + "d");
+		for (int i = 2; i <= 14; i++) {
+			list.add("d" + Integer.toString(i));
 		}
 		Deck deck = new Deck(list);
 		for (int i = 0; i < texas.players.size(); i++) {
 			deck.dealHand(texas.players.get(i));
 		}
+		System.out.println("First Round");
 		System.out.println("You have " + texas.players.get(0).getHand());
-		
-		
-
+		System.out.println("Second Round");
+		deck.dealBoard(texas, 3);
+		texas.printBoard(3);		
+		System.out.println("Third Round");
+		deck.dealBoard(texas, 1);
+		texas.printBoard(4);
+		System.out.println("Final Round");
+		deck.dealBoard(texas, 1);
+		texas.printBoard(5);
+		String bestHand = texas.players.get(0).getBestHand(texas.board);
+		int highHandScore = texas.players.get(0).getHandInt(texas.players.get(0).total);
+		int index = 0;
+		for (int i = 0; i < texas.players.size(); i++) {
+			String cards = texas.players.get(i).getBestHand(texas.board);
+			if (highHandScore < texas.players.get(i).getHandInt(texas.players.get(i).total)) {
+				highHandScore = texas.players.get(i).getHandInt(texas.players.get(i).total);
+				bestHand = cards;
+				index = i;
+			}
+		}
+		if (index == 0) {
+			System.out.println("Congrats! You won with a " + bestHand);
+		} else {
+			System.out.println("Sorry, Player " + (index + 1) + " won with a " + bestHand);
+			System.out.println(texas.players.get(index).getHand());
+		}
 	}
 
 }
--- Player.java
+++ Player.java
@@ -2,59 +2,267 @@
 public class Player {
 	private String name;
 	private String[] hand;
+	private int score;
+	public String[] total;
+
 	public Player(String name) {
 		this.name = name;
 	}
+
 	public void setName(String setName) {
 		this.name = setName;
 	}
+
 	public String getName() {
 		return this.name;
 	}
+
 	public void setHand(String firstCard, String secondCard) {
 		hand = new String[2];
 		hand[0] = firstCard;
 		hand[1] = secondCard;
 	}
+
 	public String getHand() {
 		return hand[0] + " " + hand[1];
 	}
+
 	public String getBestHand(String[] tableCards) {
-		String[] total = new String[7];
+		total = new String[7];
 		total[0] = hand[0];
 		total[1] = hand[1];
-		for (int i = 2; i < 8; i++) {
+		for (int i = 2; i < 7; i++) {
 			total[i] = tableCards[i - 2];
 		}
+		total = sort(total);
+		int bestHand = getHandInt(total);
+		if (bestHand == 10) {
+			return "Royal Flush!!";
+		}
+		if (bestHand == 9) {
+			return "Straight Flush";
+		}
+		if (bestHand == 8) {
+			return "Four of a Kind";
+		}
+		if (bestHand == 7) {
+			return "Full House";
+		}
+		if (bestHand == 6) {
+			return "Flush";
+		}
+		if (bestHand == 5) {
+			return "Straight";
+		}
+		if (bestHand == 4) {
+			return "Three of a Kind";
+		}
+		if (bestHand == 3) {
+			return "Two Pair";
+		}
+		if (bestHand == 2) {
+			return "Pair";
+		}
+		if (bestHand == 1) {
+			return "High card :(";
+		}
+		return "";
 	}
-	/**
-	 * 
-	 * @param cards
-	 * @return
+
+	public static String[] sort(String[] cards) {
+		String temp;
+		for (int i = 1; i < cards.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (Integer.parseInt(cards[j].substring(1)) < Integer.parseInt(cards[j - 1].substring(1))) {
+					temp = cards[j];
+					cards[j] = cards[j - 1];
+					cards[j - 1] = temp;
+				}
+			}
+		}
+		return cards;
+	}
+
+	public int getHandInt(String[] cards) {
+		if (straight(cards) && flush(cards)) {
+			if (royalFlush(cards)) {
+				// Royal flush
+				return 10;
+			} else {
+				// Straight flush
+				return 9;
+			}
+		}
+		if (flush(cards)) {
+			// Flush
+			return 6;
+		}
+		if (straight(cards)) {
+			// Straight
+			return 5;
+		}
+		if (pair(cards)) {
+			if (threeOfAKind(cards)) {
+				if (fourOfAKind(cards)) {
+					// Four of a kind
+					return 8;
+				} else if (fullHouse(cards)) {
+					// Full house
+					return 7;
+				} else {
+					// Three of a kind
+					return 4;
+				}
+			}
+			if (twoPair(cards)) {
+				// Two pair
+				return 3;
+			} else {
+				// Pair
+				return 2;
+			}
+		}
+		// if not flush nor straight nor pair, then high cards.
+		return 1;
+	}
+
+	/*
+	 * test for each hand an
 	 */
 	public boolean royalFlush(String[] cards) {
-		
+		if (straight(cards) && flush(cards)) {
+			int count = 0;
+			for (int i = 0; i < cards.length; i++) {
+				if (cards[i].substring(1).equals("10")) {
+					count++;
+				}
+				if (cards[i].substring(1).equals("11")) {
+					count++;
+				}
+				if (cards[i].substring(1).equals("12")) {
+					count++;
+				}
+				if (cards[i].substring(1).equals("13")) {
+					count++;
+				}
+				if (cards[i].substring(1).equals("14")) {
+					count++;
+				}
+			}
+			if (count == 5) {
+				return true;
+			}
+		}
+
+		return false;
 	}
+
 	public boolean straightFlush(String[] cards) {
-		
+		return straight(cards) && flush(cards);
 	}
+
+	public boolean pair(String[] cards) {
+		int count = 0;
+		for (int i = 0; i < cards.length; i++) {
+			for (int j = 0; j < cards.length; j++) {
+				if (i != j && cards[i].substring(1).equals(cards[j].substring(1))) {
+					count++;
+					break;
+				}
+			}
+		}
+		if (count > 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	public boolean fourOfAKind(String[] cards) {
-		
+		int count = 0;
+		for (int i = 0; i < cards.length; i++) {
+			for (int j = 0; j < cards.length; j++) {
+				if (i != j && cards[i].substring(1).equals(cards[j].substring(1))) {
+					count++;
+					break;
+				}
+			}
+		}
+		if (count == 4) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 	public boolean fullHouse(String[] cards) {
-		
+		return threeOfAKind(cards) && twoPair(cards);
+
 	}
+
 	public boolean flush(String[] cards) {
-		
+		int count = 0;
+		for (int i = 0; i < cards.length; i++) {
+			for (int j = 0; j < cards.length; j++) {
+				if (i != j && cards[i].substring(0, 1).equals(cards[j].substring(0, 1))) {
+					count++;
+					break;
+				}
+			}
+		}
+		if (count == 5) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 	public boolean straight(String[] cards) {
-		
+		int count = 0;
+		for (int i = 1; i < cards.length; i++) {
+			if (Integer.parseInt(cards[i - 1].substring(1)) + 1 == Integer.parseInt(cards[i].substring(1))) {
+				count++;
+			}
+		}
+		if (count >= 4) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 	public boolean threeOfAKind(String[] cards) {
-		
+		int count = 0;
+		for (int i = 0; i < cards.length; i++) {
+			for (int j = 0; j < cards.length; j++) {
+				if (i != j && cards[i].substring(1).equals(cards[j].substring(1))) {
+					count++;
+					break;
+				}
+			}
+		}
+		if (count == 3) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 	public boolean twoPair(String[] cards) {
-		
+		int count = 0;
+		for (int i = 1; i < cards.length; i++) {
+			if (Integer.parseInt(cards[i - 1].substring(1)) == Integer.parseInt(cards[i].substring(1))) {
+				count++;
+				if (i != cards.length - 1) {
+					i++;
+				}
+			}
+		}
+		if (count >= 2) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
 }
